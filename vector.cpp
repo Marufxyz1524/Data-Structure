342c6a1a91cc393b0bf248ef009a2ba7c9a4cc55
Maruf109 <marufxyz1524@gmail.com>
2026-06-25 22:03:19 +0600

    Data structure

---
+++ vector.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // vector<int> v;     // intialziton-1
+    // vector<int> v(10); //type 2
+    // vector<int> v(10, 2);
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << endl;
+    // } // type 3
+
+    // vector<int> v(10, 2);
+    // vector<int> v2(v);
+    // for (int i = 0; i < v2.size(); i++)
+    // {
+    //     cout << v2[i] << " ";
+    // }
+
+    // cout << v.size() << endl;
+    return 0;
+}
